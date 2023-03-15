80a5afc443eb7439bc16b8c5421116fa850952d8
prashant5nov <prashantvelocity123@gmail.com>
2023-03-15 22:32:30 +0530

    isert into db using sqlalchemy

--- alchemy3.py
+++ alchemy3.py
@@ -19,7 +19,6 @@ def get_engine():
 if __name__ == "__main__":
     engine = get_engine()
     # engine.connect()
-    breakpoint()
     meta = MetaData()
 
     # definition of table
@@ -35,10 +34,27 @@ if __name__ == "__main__":
     # actual creation of table using sqlalchemy (ORM)
     meta.create_all(bind=engine)
 
-
     # insert records into table
     statement_1 = books.insert().values(
         bookId=1, book_price=12.2, genre="fiction", book_name="old age"
     )
+    statement_2 = books.insert().values(
+        bookId=2, book_price=125.2, genre="non-fiction", book_name="random"
+    )
+    statement_3 = books.insert().values(
+        bookId=3, book_price=152.2, genre="fiction", book_name="harry Potter"
+    )
+    statement_4 = books.insert().values(
+        bookId=4, book_price=121.2, genre="non-fiction", book_name="monk who sold ferraru"
+    )
+    statement_5 = books.insert().values(
+        bookId=5, book_price=124.2, genre="fiction", book_name="sapiens"
+    )
 
-    engine.execute(statement_1)
+    with engine.connect() as conn:
+        result = conn.execute(statement_1)
+        result = conn.execute(statement_2)
+        result = conn.execute(statement_3)
+        result = conn.execute(statement_4)
+        result = conn.execute(statement_5)
+        conn.commit()
